f4438cfc6a95da5c49bb1fa9e68bca03c8b0d8d4
YttriumZr <238953683+YttriumZr@users.noreply.github.com>
2025-10-19 13:32:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from src.model.equation import conic
+import src.calculate.family
+from loguru import logger
+
+
 def main():
-    print("Hello from conic-section-coordinate-geometry!")
+    ita = conic(
+        coeff_y2=1,
+        coeff_x2=1,
+        coeff_xy=0,
+        coeff_y=1,
+        coeff_x=1,
+        constant=1,
+    )
+
+    logger.info(ita.readable_equation())
+
+    logger.success(src.calculate.family.is_circle(eq=ita))
 
 
 if __name__ == "__main__":
